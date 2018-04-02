30b9d46c39bab89374b27781cb83ca4a090b58a6
Felipe Del Valle - FVL <felipe.valle@iteris.local>
2018-04-02 16:37:51 -0300

    yt search test

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,12 +2,26 @@ import React, { Component } from 'react';
 
 class SearchBar extends Component {
 
+    constructor(props) {
+        super(props);
+
+        this.state = { term: '' };
+
+        this.onInputChange = this.onInputChange.bind(this);
+    }
+
     render() {
-        return <input onChange={this.onInputChange} />;
+        return (
+            <div>
+                <input 
+                    value={ this.state.term }
+                    onChange={ this.onInputChange } />
+            </div>
+        );
     }
 
     onInputChange(event) {
-        console.log(event.target.value);
+        this.setState({ term: event.target.value });
     }
 
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyDdNdJ6yRQ8yrmjWRtf6DSCSUPkXjHfp48';
+
+YTSearch({key: API_KEY, term: 'surboards'}, function(data) {
+    console.log(data);
+});
 
 const App = () => {
     return (
